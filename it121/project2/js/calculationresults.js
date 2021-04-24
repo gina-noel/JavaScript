4ab44b4eed46a5f3996d0a3d42f584c07986c615
gina-noel <Gina.Noel@seattlecolleges.edu>
2021-04-23 17:59:21 -0700

    Project 2

---
+++ it121/project2/js/calculationresults.js
@@ -0,0 +1,28 @@
+// variables
+var name;
+var firstNumber;
+var secondNumber;
+var numberTotals;
+
+// get user input from prompts
+name = prompt("Let's play the sum game.\nPlease enter your name");
+firstNumber = parseInt(prompt("Enter first number"));
+secondNumber = parseInt(prompt("Enter first number"));
+
+// call function
+displayUserInputs(name, firstNumber, secondNumber);
+
+//function
+function displayUserInputs(name, firstNumber, secondNumber) {
+    if (name != "" && name === null && !isNaN(firstNumber) && !isNaN(secondNumber)) {
+        numberTotals = firstNumber + secondNumber;
+        document.getElementById("result").textContent =
+            "Hi " + name + "! " + "The sum of " +
+            firstNumber + " + " +
+            secondNumber + " is : " +
+            numberTotals;
+    } else {
+        document.getElementById("result").textContent = "Oops, your input was not received properly. Please start over";
+    }
+}
+
